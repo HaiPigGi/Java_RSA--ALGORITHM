0b30167e8a3002b52f153b29d981e416e7a8ef6d
HaiPigGi <leonardobryan32@gmail.com>
2023-12-17 22:35:32 +0700

    updte code for an logic RSA

--- src/RSA.java
+++ src/RSA.java
@@ -1,3 +1,40 @@
+import java.math.BigInteger;
+
 public class RSA {
+
+    private static BigInteger p = BigInteger.valueOf(47);
+    private static BigInteger q = BigInteger.valueOf(71);
+     private static BigInteger n = p.multiply(q);
+    private static BigInteger phi = (p.subtract((BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE)));
+    public static void main(String[] args) {
+        //Pilih Kunci Publik e
+        BigInteger e = BigInteger.valueOf(79);
+        
+        // Hitung Kunci Private d
+        BigInteger d = phi.modInverse(e);
+
+        // pesan yang akan di enkripsi
+
+        String plainText = "HARI INI";
+        byte [] plainTextBytes= plainText.getBytes();
+
+        //enkripsi pesan
+        byte[] cipherTextBytes = encrypt(plainTextBytes, e, n);
+    }
+
+
+
+    private static byte [] encrypt (byte[] plainTextBytes, BigInteger e, BigInteger n) {
+        BigInteger CipherText = new BigInteger(1, plainTextBytes);
+        CipherText= CipherText.modPow(e, n);
+        return CipherText.toByteArray();
+    }
+
+     private static byte [] decypt (byte[] cipherTextBytes, BigInteger d, BigInteger n) {
+        BigInteger plainText = new BigInteger(1, cipherTextBytes);
+        plainText= plainText.modPow(d, n);
+        return plainText.toByteArray();
+    }
     
+
 }
